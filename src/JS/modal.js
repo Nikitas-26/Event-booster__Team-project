db092bc6a00ba23076c71135053e1a507f417ce7
Nick Arabadzhi <nikita.arabadji@gmail.com>
2022-01-21 10:54:02 +0200

    update

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,14 +1,14 @@
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector('[data-franshize-open]'),
-//       closeModalBtn: document.querySelector('[data-franshize-close]'),
-//       modal: document.querySelector('[data-franshize]'),
-//     };
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('.photo-card'),
+      closeModalBtn: document.querySelector('.modal-close-js'),
+      modal: document.querySelector('.modal-js'),
+    };
 
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//     function toggleModal() {
-//       refs.modal.classList.toggle('is-hidden');
-//     }
-//   })(); 
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })(); 
